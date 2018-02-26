43e22673d1c116cbfbe954e533d2e46ffa856898
EugeneFaceControl <eugenefacecontrol@gmail.com>
2018-02-26 22:59:27 +0300

    Visual part

--- Files/addToDatabase.php
+++ Files/addToDatabase.php
@@ -7,8 +7,8 @@ $dbname = $ini_array["dbname"];
 $tablename = $ini_array["tablename"];
 // Create connection
 $conn = new mysqli($servername, $username, $password, $dbname);
-// Check connection
 
+// Check connection
 if ($conn->connect_error) {
 	die("Connection failed: " . $conn->connect_error);
 }
@@ -49,7 +49,7 @@ if ($conn->query($sql) === TRUE) {
 
 	echo "Добавлено:<br>";
 	echo $_POST["familyMember"];
-	echo "<br><br><a href='https://eugenesheyma.000webhostapp.com/". $tablename . "'>Вернуться назад</a>";
+	echo "<br><br><a href='https://$_SERVER[HTTP_HOST]/". $tablename . "'>Вернуться назад</a>";
 } else {
 
 	echo "Error: " . $sql . "<br>" . $conn->error;
